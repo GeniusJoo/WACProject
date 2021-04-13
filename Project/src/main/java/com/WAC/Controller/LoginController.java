cfc81e17b7909c09b69aebf3d3d0f1ded296eddf
Jiwon Ju <wnwldnjs1@gmail.com>
2021-04-13 17:05:49 +0900

    로그인 2차 업데이트

--- Project/src/main/java/com/WAC/Controller/LoginController.java
+++ Project/src/main/java/com/WAC/Controller/LoginController.java
@@ -1,5 +1,20 @@
 package com.WAC.Controller;
 
-public class LoginController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.WAC.Service.LoginService;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	LoginService loginService;
+	
+	@GetMapping(value = "/")
+	public String Main() {
+		return "home";
+	}
+	
 }
