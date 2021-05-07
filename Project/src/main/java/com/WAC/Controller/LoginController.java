2efe52e1afc25338045d7dfc6b8b6015a299a6e8
Jiwon Ju <wnwldnjs1@gmail.com>
2021-05-07 16:41:08 +0900

    DB 커넥션 추가 및 테스트 예정, 이미지 및 HTML 추가

--- Project/src/main/java/com/WAC/Controller/LoginController.java
+++ Project/src/main/java/com/WAC/Controller/LoginController.java
@@ -3,10 +3,11 @@ package com.WAC.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.WAC.Service.LoginService;
 
-@Controller
+@RestController
 public class LoginController {
 	
 	@Autowired
@@ -14,7 +15,8 @@ public class LoginController {
 	
 	@GetMapping(value = "/")
 	public String Main() {
-		return "home";
+		System.out.println("test");
+		return "testtt";
 	}
 	
 }
