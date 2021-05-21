1a634d9810533f74d8e45502e9f25ab0579a6623
yoojw1021 <34273979+yoojw1021@users.noreply.github.com>
2021-05-21 22:07:47 +0900

    Add files via upload

---
+++ Project/src/main/resources/static/zagazindan.js
@@ -0,0 +1,63 @@
+// 체크 박스 name
+const inputNames = ['skinColor','accessary','eyeColor','hairColor','sunlight'];
+
+// 체크 박스 클릭 event 추가
+inputNames.forEach((inputName) => {
+    let checkBoxses = document.querySelectorAll(`input[name=${inputName}]`)
+    checkBoxses.forEach((checkBox) => {
+        checkBox.addEventListener('click', () => {
+            initCheckBox(inputName);
+            checkBox.checked = true;
+        })
+    })
+})
+
+// 체크 박스 초기화 함수
+const initCheckBox = (inputName) => {
+    let checkBoxses = document.querySelectorAll(`input[name=${inputName}]`)
+    checkBoxses.forEach((checkBox) => {
+        checkBox.checked = false;
+    })
+}
+
+// 체크 박스 확인 함수
+const checkCheckBox = () => {
+    for (let i = 0; i < inputNames.length; i++) {
+         if(document.querySelectorAll(`input[name=${inputNames[i]}]:checked`).length === 0){
+            return false;
+        }
+        
+    }
+    return true;
+}
+
+// 체크 박스 계산 함수
+const calCheckBox = () => {
+    let warmCnt = 0, coolCnt = 0;
+
+    for (let i = 0; i < inputNames.length; i++) {
+        let checkInput = document.querySelectorAll(`input[name=${inputNames[i]}]:checked`)[0]
+        if(checkInput === document.querySelector(`input[name=${inputNames[i]}]:nth-child(1)`)){
+            warmCnt++;
+        }else{
+            coolCnt++;
+        }
+   }
+   
+   return warmCnt > coolCnt ? ["warm",warmCnt] : ["cold",coolCnt];
+}
+
+
+// 체크 박스 제출 함수
+const submitBnt = document.getElementById('submit')
+submitBnt.addEventListener('click', () => {
+    if(checkCheckBox()){
+        let result = calCheckBox()
+        alert(result[0] == "warm" ?"당신의 퍼스널 컬러는 웜톤입니다.":"당신의 퍼스널 컬러는 쿨톤입니다.")
+    } else{
+        alert("체크박스를 모두 선택해주세요!");
+    }
+})
+
+
+
