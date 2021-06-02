3fd018bdd730b920caee3d299ab7ef60c1c0b80f
DESKTOP-8BTLTQU\HanSeung <gkstmd1014@naver.com>
2021-06-03 03:22:18 +0900

    자가진단 수정

--- Project/src/main/resources/static/zagazindan.js
+++ Project/src/main/resources/static/zagazindan.js
@@ -1,30 +1,10 @@
 // 체크 박스 name
 const inputNames = ['skinColor','accessary','eyeColor','hairColor','sunlight'];
 
-
-// 체크 박스 클릭 event 추가
-inputNames.forEach((inputName) => {
-    let checkBoxses = document.querySelectorAll(`input[name=${inputName}]`)
-    checkBoxses.forEach((checkBox) => {
-        checkBox.addEventListener('click', () => {
-            initCheckBox(inputName);
-            checkBox.checked = true;
-        })
-    })
-})
-
-// 체크 박스 초기화 함수
-const initCheckBox = (inputName) => {
-    let checkBoxses = document.querySelectorAll(`input[name=${inputName}]`)
-    checkBoxses.forEach((checkBox) => {
-        checkBox.checked = false;
-    })
-}
-
 // 체크 박스 확인 함수
-const checkCheckBox = () => {
-    for (let i = 0; i < inputNames.length; i++) {
-         if(document.querySelectorAll(`input[name=${inputNames[i]}]:checked`).length === 0){
+function checkCheckBox() {
+    for (var i = 0; i < inputNames.length; i++) {
+        if(document.querySelectorAll(`input[name=${inputNames[i]}]:checked`).length === 0){
             return false;
         }
         
@@ -33,11 +13,11 @@ const checkCheckBox = () => {
 }
 
 // 체크 박스 계산 함수
-const calCheckBox = () => {
-    let warmCnt = 0, coolCnt = 0;
+function calCheckBox() {
+    var warmCnt = 0, coolCnt = 0;
 
-    for (let i = 0; i < inputNames.length; i++) {
-        let checkInput = document.querySelectorAll(`input[name=${inputNames[i]}]:checked`)[0]
+    for (var i = 0; i < inputNames.length; i++) {
+        var checkInput = document.querySelectorAll(`input[name=${inputNames[i]}]:checked`)[0]
         if(checkInput === document.querySelector(`input[name=${inputNames[i]}]:nth-child(1)`)){
             warmCnt++;
         }else{
@@ -53,7 +33,7 @@ const calCheckBox = () => {
 function submitBnt()
 {
     if(checkCheckBox()){
-        let result = calCheckBox()
+        var result = calCheckBox()
         if (result[0] == "warm")
         {
             alert("당신의 퍼스널 컬러는 웜톤입니다.");
@@ -64,10 +44,7 @@ function submitBnt()
             alert("당신의 퍼스널 컬러는 쿨톤입니다.");
             return result[0];
         }
-    } else{
+    } else {
         alert("체크박스를 모두 선택해주세요!");
     }
 }
-
-
-
