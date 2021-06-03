11471914ad7f76b7a670def62aa8c7aab5f5ea59
Jiwon Ju <wnwldnjs1@gmail.com>
2021-06-04 03:13:06 +0900

    회원정보 수정 제외 라우팅 완료
    이슈사항 확인 후 수정 요망

--- Project/src/main/java/com/WAC/Controller/Controller.java
+++ Project/src/main/java/com/WAC/Controller/Controller.java
@@ -28,4 +28,58 @@ public class Controller {
 		
 		return "intro1";
 	}
+	
+	@GetMapping(value = "/warmFeed")
+	public String WarmFeed() throws Exception {
+		
+		return "warmFeed";
+	}
+	
+	@GetMapping(value = "/coolFeed")
+	public String CoolFeed() throws Exception {
+		
+		return "coolFeed";
+	}
+	
+	@GetMapping(value = "/coolMedia")
+	public String CoolMedia() throws Exception {
+		
+		return "coolMedia";
+	}
+	
+	@GetMapping(value = "/intro2")
+	public String Intro2() throws Exception {
+		
+		return "intro2";
+	}
+	
+	@GetMapping(value = "/coolPopular")
+	public String CoolPopular() throws Exception {
+		
+		return "coolPopular";
+	}
+	
+	@GetMapping(value = "/coolOotd")
+	public String CoolOotd() throws Exception {
+		
+		return "coolOotd";
+	}
+	
+	@GetMapping(value = "/warmMedia")
+	public String WarmMedia() throws Exception {
+		
+		return "warmMedia";
+	}
+	
+	@GetMapping(value = "/warmOotd")
+	public String WarmOotd() throws Exception {
+		
+		return "warmOotd";
+	}
+	
+	@GetMapping(value = "/warmPopular")
+	public String WarmPopular() throws Exception {
+		
+		return "warmPopular";
+	}
 }
